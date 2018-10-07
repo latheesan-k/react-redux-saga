cf716c52c7e8240f64688f0f67ff1c6da295552d
Latheesan Kanesamoorthy <latheesan@outlook.com>
2018-10-07 19:51:43 +0100

    Fixed a typo.

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 
-import reducer from "./reducers.js";
+import reducer from "./reducers";
 import mySaga from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
